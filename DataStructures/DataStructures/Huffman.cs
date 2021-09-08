6e2db023fc6479dcc2cdf8273c9765fb29371cd5
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2021-09-08 08:40:57 -0600

    clases y heap

---
+++ DataStructures/DataStructures/Huffman.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Huffman
+    {
+        internal Heap<HuffmanNode> heap;
+
+        Huffman()
+        {
+
+        }
+
+        public string Compress(string text)
+        {
+
+        }
+
+        private void buildHeap(string text)
+        {
+            DoubleLinkedList<HuffmanNode> list = new DoubleLinkedList<HuffmanNode>();
+            int cant = 0;
+            while(text.Length > 0)
+            {
+                cant++;
+                int cont = 1;
+                char character = text[0];
+                text = text.Remove(0, 1);
+                for(int i = 0; i < text.Length; i++)
+                {
+                    if(text[i] == character)
+                    {
+                        text = text.Remove(i, 1);
+                        cont++;
+                    }
+                }
+                list.InsertAtEnd(new HuffmanNode(character, cont));
+            }
+
+            heap = new Heap<HuffmanNode>(cant);
+            foreach(var node in list)
+            {
+                node.frecuency = node.frecuency / cant;
+                heap.insertKey(node, node.frecuency.ToString());
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/HuffmanNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class HuffmanNode : IComparable
+    {
+        public char value;
+        public string binary_value;
+        public double frecuency;
+        HuffmanNode rigth;
+        HuffmanNode left;
+
+        public HuffmanNode(char new_value, double new_frecuency)
+        {
+            value = new_value;
+            frecuency = new_frecuency;
+            binary_value = "";
+            rigth = null;
+            left = null;
+        }
+
+        public bool leaf()
+        {
+            return (rigth == null && left == null);
+        }
+
+    }
+}
