754a438e70ecabe59ed6aaa6522a4f220bdc7ca4
danielvh01 <danielvelasquez32@gmail.com>
2021-09-25 19:44:57 -0600

    LZW 80%

--- HUFFMANN_STRING/Huffman_String/Program.cs
+++ HUFFMANN_STRING/Huffman_String/Program.cs
@@ -7,6 +7,7 @@ namespace Huffman_String
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             LZW compresor = new LZW();
@@ -18,7 +19,7 @@ namespace Huffman_String
                 if(text.Length == 0)
                 {
                     Console.WriteLine("Error, el texto esta vacio por favor intentelo de nuevo");
-                }
+                }                
             }
             while (text.Length == 0);
             Console.WriteLine("El mensaje comprimido es el siguiente:");
