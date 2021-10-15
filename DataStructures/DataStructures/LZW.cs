f8cdfaff0daacb8af2171bdf9c42d82886590493
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2021-10-14 22:40:04 -0600

    lzm changes

--- DataStructures/DataStructures/LZW.cs
+++ DataStructures/DataStructures/LZW.cs
@@ -8,21 +8,10 @@ namespace DataStructures
     {
         private Dictionary<int, string> dictionary;
         private Dictionary<string, int> dictionaryC;
-        public LZW() 
+        public LZW()
         {
             dictionary = new Dictionary<int, string>();
             dictionaryC = new Dictionary<string, int>();
-        }
-
-        
-        private int binaryToInt(string Btext)
-        {
-            int result = 0;
-            for (int i = Btext.Length - 1; i >= 0; i--)
-            {
-                result += Convert.ToInt32(Btext.Substring((Btext.Length - 1) - i, 1)) * (int)(Math.Pow(2, i));
-            }
-            return result;
         }
 
         public byte[] Compress(byte[] text)
@@ -32,7 +21,7 @@ namespace DataStructures
             int dictionaryCharCant = 0;
             string compressed = string.Empty;
             byte[] result = new byte[0];
-            
+
             if (text != null || text.Length != 0)
             {
                 for (int i = 0; i < text.Length; i++)
@@ -67,7 +56,7 @@ namespace DataStructures
                 string[] code = compressed.Split("-");
                 for (int i = 0; i < code.Length; i++)
                 {
-                    binaryCode += Convert.ToString(Convert.ToInt32(code[i]),2).PadLeft(cantByte,'0');
+                    binaryCode += Convert.ToString(Convert.ToInt32(code[i]), 2).PadLeft(cantByte, '0');
                 }
                 int cant = 8 - binaryCode.Length % 8;
                 while (cant > 0)
@@ -77,7 +66,7 @@ namespace DataStructures
                 }
                 result = new byte[2 + dictionaryCharCant + (binaryCode.Length / 8)];
                 result[0] = (byte)cantByte;
-                result[1] = (byte)(dictionaryCharCant-1);
+                result[1] = (byte)(dictionaryCharCant - 1);
                 int count = 0;
                 foreach (var x in dictionaryC.Keys)
                 {
@@ -93,7 +82,7 @@ namespace DataStructures
                 }
                 for (int i = 0; i < binaryCode.Length / 8; i++)
                 {
-                    result[i + 2 + dictionaryCharCant] = (byte)binaryToInt(binaryCode.Substring(i * 8, 8));
+                    result[i + 2 + dictionaryCharCant] = (byte)Convert.ToInt32(binaryCode.Substring(i * 8, 8), 2);
                 }
                 return result;
 
@@ -109,13 +98,13 @@ namespace DataStructures
             string result = "";
             int bytesPerCharacter = compressedText[0];
             int alphabethLength = (compressedText[1]) + 1;
-            
+
             for (int i = 0; i < alphabethLength; i++)
             {
                 dictionary.Add(i + 1, ((char)(compressedText[2 + i])).ToString());
             }
             string binaryText = "";
-            for(int i = 2 + alphabethLength; i < compressedText.Length; i++)
+            for (int i = 2 + alphabethLength; i < compressedText.Length; i++)
             {
                 string x = Convert.ToString(compressedText[i], 2);
                 binaryText += x.PadLeft(8, '0');
@@ -125,13 +114,13 @@ namespace DataStructures
             string chain = "";
             string character;
 
-            previous = binaryToInt(binaryText.Substring(0, bytesPerCharacter));
+            previous = Convert.ToInt32(binaryText.Substring(0, bytesPerCharacter), 2);
             character = dictionary[previous];
             result += character;
-            for (int i = 1; i < binaryText.Length/bytesPerCharacter; i++)
+            for (int i = 1; i < binaryText.Length / bytesPerCharacter; i++)
             {
-                current = binaryToInt(binaryText.Substring(i*bytesPerCharacter, bytesPerCharacter));
-                if (current!= 0)
+                current = Convert.ToInt32(binaryText.Substring(i * bytesPerCharacter, bytesPerCharacter), 2);
+                if (current != 0)
                 {
                     if (!dictionary.ContainsKey(current))
                     {
@@ -143,9 +132,9 @@ namespace DataStructures
                         chain = dictionary[current];
                     }
                     result += chain;
-                    character = chain.Substring(0,1);
+                    character = chain.Substring(0, 1);
                     dictionary.Add(dictionary.Count + 1, dictionary[previous] + character);
-                    previous = current; 
+                    previous = current;
                 }
                 else
                 {
